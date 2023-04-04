b702a1f1419815a3a7a3735f81715c89911ccaf7
Mathieu Enault <me94110@gmail.com>
2023-04-04 16:54:25 +0200

    progress on contact form

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     formats: ["image/webp"],
   },
+  env: {
+    serviceId: "service_np1ni3r",
+    templateId: "template_f253loq",
+    userId: "LnHga61xZ8ji6B56V",
+  },
 };
 
 module.exports = nextConfig;
--- src/components/Form/ContactForm.js
+++ src/components/Form/ContactForm.js
@@ -1,129 +1,132 @@
-import Head from "next/head";
-import { useState } from "react";
-import TextArea from "./TextArea";
-import TextInputs from "./TextInputs";
+import { useForm } from "react-hook-form";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 export default function ContactForm() {
-  const [name, setName] = useState({
-    name: "name",
-    label: "Name",
-    value: "",
-    focus: false,
-    type: "text",
-  });
-  const [email, setEmail] = useState({
-    name: "email",
-    label: "Email",
-    value: "",
-    focus: false,
-    type: "email",
-  });
-  const [message, setMessage] = useState({
-    name: "message",
-    label: "Message",
-    value: "",
-    focus: false,
-  });
-  const [submitted, setSubmitted] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Sending");
-    let data = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    fetch("/api/contact", {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    }).then((res) => {
-      console.log("Response received");
-      if (res.status === 200) {
-        console.log("Response succeeded!");
-        setSubmitted(true);
-        setName("");
-        setEmail("");
-        setMessage("");
-      }
+  const toastifySuccess = () => {
+    toast("Form sent!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: "submit-feedback success",
+      toastId: "notifyToast",
     });
   };
+  const onSubmit = async (data) => {
+    const { name, email, message } = data;
+    try {
+      const templateParams = {
+        name,
+        email,
+        message,
+      };
+      console.log(
+        "REACT_APP_SERVICE_ID =",
+        process.env.serviceId,
+        "REACT_APP_TEMPLATE_ID =",
+        process.env.templateId,
+        templateParams,
+        "REACT_APP_USER_ID =",
+        process.env.userId
+      );
+      await emailjs.send(
+        process.env.serviceId,
+        process.env.templateId,
+        templateParams,
+        process.env.userId
+      );
 
-  const handleFocus = (e) => {
-    const inputName = e.target.name;
-    if (inputName === "name") {
-      setName({ ...name, focus: true });
-    } else if (inputName === "email") {
-      setEmail({ ...email, focus: true });
-    } else {
-      setMessage({ ...message, focus: true });
-    }
-  };
-
-  const handleBlur = (e) => {
-    const inputName = e.target.name;
-    if (inputName === "name") {
-      setName({ ...name, focus: false });
-    } else if (inputName === "email") {
-      setEmail({ ...email, focus: false });
-    } else {
-      setMessage({ ...message, focus: false });
-    }
-  };
-
-  const handleChange = (e) => {
-    const inputName = e.target.name;
-    const inputValue = e.target.value;
-    if (inputName === "name") {
-      setName({ ...name, value: inputValue });
-    } else if (inputName === "email") {
-      setEmail({ ...email, value: inputValue });
-    } else {
-      setMessage({ ...message, value: inputValue });
+      reset();
+      toastifySuccess();
+    } catch (e) {
+      console.log(e);
     }
   };
 
   return (
-    <div className="w-[600px] border rounded-lg border-solid border-black shadow-[0_2px_10px_rgba(124,124,124,0.6)] overflow-hidden  p-2 flex flex-col justify-center items-center sm:w-full">
-      <form className="w-full p-10 flex-1 flex flex-col justify-center items-center sm:p-0">
-        <formGroup className="h-1/2 my-10 flex flex-col flex-1 w-full">
-          <TextInputs
-            {...name}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            onChange={handleChange}
-          />
-        </formGroup>
+    <>
+      <div className="w-[600px] border rounded-lg border-solid border-black shadow-[0_2px_10px_rgba(124,124,124,0.6)] overflow-hidden  p-2 flex flex-col justify-center items-center sm:w-full">
+        <form
+          className="w-full p-10 flex-1 flex flex-col justify-center items-center sm:p-0"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+        >
+          <div className="h-1/2 my-10 flex flex-col flex-1 w-full relative">
+            <input
+              type="text"
+              name="name"
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: "Il faut indiquer votre nom",
+                },
+                maxLength: {
+                  value: 30,
+                  message:
+                    "La valeur du champs ne doit pas dépasser 30 caractères",
+                },
+              })}
+              className="focus:border-4 focus:border-black focus:mb-4 focus:block focus:w-full focus:p-4 focus:rounded focus:outline-none focus:text-base border-2 border border-black mb-4 block w-full p-4 rounded duration-150 ease outline-none text-base"
+              placeholder="Nom"
+            />
 
-        <formGroup className="h-1/2 my-10 flex flex-col flex-1 w-full">
-          <TextInputs
-            {...email}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            onChange={handleChange}
-          />
-        </formGroup>
+            {errors.name && (
+              <span className="errorMessage">{errors.name.message}</span>
+            )}
+          </div>
 
-        <formGroup className="h-1/2 my-10 flex flex-col flex-1 w-full">
-          <TextArea
-            {...message}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            onChange={handleChange}
+          <div className="h-1/2 my-10 flex flex-col flex-1 w-full">
+            <input
+              type="email"
+              name="email"
+              {...register("email", {
+                required: true,
+                pattern:
+                  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              })}
+              className="focus:border-4 focus:border-black focus:mb-4 focus:block focus:w-full focus:p-4 focus:rounded focus:outline-none focus:text-base border-2 border border-black mb-4 block w-full p-4 rounded duration-150 ease outline-none text-base"
+              placeholder="Email"
+            ></input>
+            {errors.email && (
+              <span className="errorMessage">
+                Indiquez une adresse email valide
+              </span>
+            )}
+          </div>
+
+          <div className="h-1/2 my-10 flex flex-col flex-1 w-full relative">
+            <textarea
+              name="message"
+              {...register("message", {
+                required: true,
+              })}
+              className="h-32 w-full mb-4 block focus:border-black focus:border-4  p-4 rounded transition duration-150 ease outline-none text-base focus:text-base border-2 border-black"
+              placeholder="Message"
+            ></textarea>
+            {errors.message && (
+              <span className="errorMessage">
+                Il faut indiquer un message !
+              </span>
+            )}
+          </div>
+          <input
+            type="submit"
+            className="block w-full bg-[#000] border-none rounded p-4 text-base text-white font-semibold hover:cursor-pointer"
           />
-        </formGroup>
-        <input
-          type="submit"
-          className="block w-full bg-[#000] border-none rounded p-4 text-base text-white font-semibold hover:cursor-pointer"
-          onClick={(e) => {
-            handleSubmit(e);
-          }}
-        />
-      </form>
-    </div>
+        </form>
+      </div>
+      <ToastContainer />
+    </>
   );
 }
